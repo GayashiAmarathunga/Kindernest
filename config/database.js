9e67fe6d59cb14e42a198911414a1c5c5df5bba9
Aman <amanahamed770@gmail.com>
2024-02-27 11:32:37 +0530

    Database Update

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mysql from "mysql2";
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "POIU098qwerty7654321MNB",
   database: "kindergarten_management",

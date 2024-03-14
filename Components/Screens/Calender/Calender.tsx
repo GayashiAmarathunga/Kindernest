6a40f6256aacf5c8aa7c59874357417ee258beae
aman1788 <100472155+aman1788@users.noreply.github.com>
2024-03-14 23:36:16 +0530

    Added custom day with schedule lines

--- Components/Screens/Calender/Calender.tsx
+++ Components/Screens/Calender/Calender.tsx
@@ -1,6 +1,16 @@
+import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import {Calendar} from 'react-native-calendars';
-import Day from './Day';
+import {Calendar, LocaleConfig, CalendarProps} from 'react-native-calendars';
+import CustomDay from './CustomDay';
+
+const scheduleData: {[key: string]: string[]} = {
+  '2024-03-01': ['class'],
+  '2024-03-04': ['task'],
+  '2024-03-05': ['class'],
+  '2024-03-13': ['task'],
+  '2024-03-20': ['task'],
+  '2024-03-29': ['class'],
+};
 
 const Calender = () => {
   return (
@@ -21,16 +31,31 @@ const Calender = () => {
             arrowColor: 'blue',
             monthTextColor: '#9100EB',
             textDayFontFamily: 'Poppins-Medium',
+            textDayFontSize: styles.day.fontSize,
             textMonthFontFamily: 'Poppins-Medium',
             textDayHeaderFontFamily: 'Poppins-Medium',
+            textDayHeaderFontSize: styles.dayHeader.fontSize,
           }}
           hideArrows={true}
+          dayComponent={({date}) => (
+            <CustomDay
+              date={date}
+              schedules={scheduleData[date?.dateString || ''] || []}
+            />
+          )}
         />
       </View>
+
+      {/* Separator (a line) */}
+      <View
+        style={{
+          width: '80%',
+          borderBottomWidth: 1,
+          borderBottomColor: '#838383',
+          marginTop: 30,
+        }}
+      />
     </SafeAreaView>
-    // <View style={styles.container}>
-    //   <Text style={styles.dates}>MAR</Text>
-    // </View>
   );
 };
 
@@ -45,14 +70,11 @@ const styles = StyleSheet.create({
   calenderContainer: {
     width: 350,
     paddingTop: 20,
-    // borderColor: 'red',
-    // borderWidth: 1,
-    // borderStyle: 'solid',
   },
-  dates: {
-    textAlign: 'center',
+  dayHeader: {
+    fontSize: 12,
+  },
+  day: {
     fontSize: 16,
-    fontFamily: 'Poppins-Medium',
-    color: '#9100EB',
   },
 });

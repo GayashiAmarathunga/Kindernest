c8b9651272b4609684621e6ae0c2c074a7eb6893
GayashiAmarathunga <gayashi.20221460@iit.ac.lk>
2024-04-01 05:09:11 +0530

    Home Teacher

--- Components/Screens/Home.tsx
+++ Components/Screens/Home.tsx
@@ -4,3 +4,19 @@ import { supabase } from '../../lib/supabase'
 import { Session } from '@supabase/supabase-js'
 
 const Home = ({navigation}) => {
+
+    const [session, setSession] = useState<Session | null>(null)
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+          setSession(session)
+        })
+
+        supabase.auth.onAuthStateChange((_event, session) => {
+          setSession(session)
+        })
+
+
+      }, [])
+
+      

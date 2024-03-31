3e1a0a4504133b294ba2834f271e88d85b033b1f
NadeepaThemiya <nadeepathushal@gmail.com>
2024-04-01 03:30:11 +0530

    Update ParentSignup.tsx

--- Frontend/Components/Screens/ParentSignup.tsx
+++ Frontend/Components/Screens/ParentSignup.tsx
@@ -12,3 +12,37 @@ const ParentSignup = ({ navigation }) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+
+
+
+
+    async function handleSignup() {
+        if (password !== confirmPassword) {
+            Alert.alert("Passwords do not match")
+            return;
+        }
+
+        const { data, error } = await supabase.auth.signUp(
+            {
+                email: email,
+                password:  password,
+                options: {
+                    data: {
+                        role: 'parent',
+                        parent_name: parentName,
+                        child_name: childName,
+                        kindergarten_level: kindergartenLevel,
+                        class: classLevel,
+                        date_of_birth: dateOfBirth
+                    }
+                }
+            }
+        )
+
+        if (error) {
+            Alert.alert(error.message)
+        }
+        if (!error) {
+            navigation.navigate('PHome')
+        }
+    }

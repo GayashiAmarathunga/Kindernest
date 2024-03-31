a7713db4ab5ca2301828aca347939ffd3bd3d24c
NadeepaThemiya <nadeepathushal@gmail.com>
2024-04-01 03:21:31 +0530

    Update TeacherSignUp.tsx

--- Frontend/Components/Screens/TeacherSignUp.tsx
+++ Frontend/Components/Screens/TeacherSignUp.tsx
@@ -1,3 +1,18 @@
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { supabase } from '../../lib/supabase';
+
+const TeacherSignUp = ({ navigation }) => {
+  const [teacherName, setTeacherName] = useState('');
+  const [kindergartenLevel, setKindergartenLevel] = useState('');
+  const [gender, setGender] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSignUp = async () => {
+    
+    if (password !== confirmPassword) {
+      Alert.alert("Passwords do not match")
+      return;
+  }

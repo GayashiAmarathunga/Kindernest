ba415981cf62505fa040e7ead2f4c978a6a07295
SasmithaLochana <sasmithalochana807695@gmail.com>
2024-04-01 02:46:31 +0530

    Update Progressparent.tsx

--- Frontend/Components/Screens/Progressparent.tsx
+++ Frontend/Components/Screens/Progressparent.tsx
@@ -22,3 +22,55 @@ interface ProgressData {
   created: string;
   __v: number;
 }
+
+const ProgressParent = () => {
+
+    const [session, setSession] = useState<Session | null>(null)
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+          setSession(session)
+        })
+    
+        supabase.auth.onAuthStateChange((_event, session) => {
+          setSession(session)
+        })
+    
+     
+      }, [])
+
+      const [progress, setProgress] = useState<ProgressData | null>(null);
+
+    useEffect(() => {
+      const fetchProgress = async () => {
+        try {
+          const response = await axios.get(`${process.env.API_URL}/progress/${session?.user.email}`);
+          console.log('Progress data:', response.data);
+          setProgress(response.data);
+        } catch (error) {
+          console.error('Error fetching progress:', error);
+        }
+      };
+  
+      fetchProgress();
+    }, []);
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Image
+            style={{width: 393, height: 80}}
+            source={require('../images/prog.png')}
+          />
+  
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 30,
+              marginTop: 0,
+              fontWeight: '500',
+            }}>
+            Progress
+          </Text>
+
+          

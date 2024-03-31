d4a44bbd49180431260c464c2726f4b50f4f2ea1
GayashiAmarathunga <gayashi.20221460@iit.ac.lk>
2024-03-31 17:29:28 +0530

    online teacher1

--- Frontend/Components/Screens/OnlineClassTeacher.tsx
+++ Frontend/Components/Screens/OnlineClassTeacher.tsx
@@ -13,6 +13,26 @@ import {
   import { Session } from '@supabase/supabase-js';
   import { supabase } from '../../lib/supabase';
   
-  const OnlineClassTeacher = () => {}
+  const OnlineClassTeacher = () => {
+
+    const [classSession, setClassSession] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [classSubject, setClassSubject] = useState('');
+    const [teacherName, setTeacherName] = useState('');
+  
+    const [session, setSession] = useState<Session | null>(null)
+  
+    useEffect(() => {
+      supabase.auth.getSession().then(({ data: { session } }) => {
+        setSession(session)
+      })
+  
+      supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session)
+      })
+  
+  
+    }, [])
 
   return

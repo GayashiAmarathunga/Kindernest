2942a8c038bc491011572a8ef1770286184a3aa1
ChamithSILVA <151826173+ChamithSILVA@users.noreply.github.com>
2024-04-01 00:35:22 +0530

    ContactTaecher3

--- Frontend/Components/Screens/ContactTeacher.tsx
+++ Frontend/Components/Screens/ContactTeacher.tsx
@@ -75,3 +75,18 @@ const ContactTeacher = () => {
       );
     };
     
+    const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          justifyContent: 'center',
+          padding: 20,
+        },
+        input: {
+          height: 100,
+          marginBottom: 20,
+          borderWidth: 1,
+          padding: 10,
+        },
+      });
+      
+      export default ContactTeacher;

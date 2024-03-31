ec921eab5e79bef4e84073ea4b2a19eab4fe715d
GayashiAmarathunga <gayashi.20221460@iit.ac.lk>
2024-03-31 17:24:09 +0530

    onlineTeacher

---
+++ Frontend/Components/Screens/OnlineClassTeacher.tsx
@@ -0,0 +1,18 @@
+import {
+    View,
+    Text,
+    SafeAreaView,
+    Image,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+    Alert,
+  } from 'react-native';
+  import React, { useEffect, useState } from 'react';
+  import axios from 'axios';
+  import { Session } from '@supabase/supabase-js';
+  import { supabase } from '../../lib/supabase';
+  
+  const OnlineClassTeacher = () => {}
+
+  return

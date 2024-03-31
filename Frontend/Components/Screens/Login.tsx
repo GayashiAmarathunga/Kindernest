970a69959593751cc602ca372b44b79bacf743fd
NadeepaThemiya <nadeepathushal@gmail.com>
2024-04-01 03:16:53 +0530

    Update Login.tsx

--- Frontend/Components/Screens/Login.tsx
+++ Frontend/Components/Screens/Login.tsx
@@ -50,3 +50,24 @@ const Login = ({ navigation }) => {
     //if (!error) navigation.navigate('PHome');
 
   }
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image style={styles.bbyimage} source={require("../images/bby2.png")}></Image>
+        <Text style={styles.text}>Welcome to</Text>
+        <Text style={styles.text1}>KinderNest</Text>
+        <Text style={styles.text2}>~ Streamline KMS for teachers & parents ~</Text>
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <TextInput style={styles.input} placeholderTextColor={'black'} placeholder='Email' onChangeText={(text) => setEmail(text)}></TextInput>
+        <TextInput secureTextEntry style={styles.input} placeholderTextColor={'black'} placeholder='Password' onChangeText={(text) => setPassword(text)}></TextInput>
+        <TouchableOpacity onPress={() => signInWithEmail()} style={styles.button}><Text style={{ fontSize: 20, padding: 10, textAlign: 'center', fontWeight: 'bold' }}>Login</Text></TouchableOpacity>
+      </View>
+      <View style={{ marginTop: 20, alignItems: 'center' }}>
+        <TouchableOpacity onPress={() => navigation.navigate('FPassword')}><Text style={{ fontSize: 20, padding: 5, textAlign: 'center', fontWeight: 'bold' }}>Forgot Password</Text></TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Register')} style={styles.button}><Text style={{ fontSize: 20, padding: 10, textAlign: 'center', fontWeight: 'bold', }}>Signup</Text></TouchableOpacity>
+        <TouchableOpacity><Text style={{ fontSize: 15, padding: 5, textAlign: 'center', fontWeight: 'bold',color: 'black', }}>Don't have an account</Text></TouchableOpacity>
+      </View>
+    </View>
+  )
+}

889aebe5367afdf5d7e3d268fcd380750c58cc28
GayashiAmarathunga <gayashi.20221460@iit.ac.lk>
2024-04-01 02:04:02 +0530

    About us

--- Frontend/Components/Screens/AboutUs.tsx
+++ Frontend/Components/Screens/AboutUs.tsx
@@ -1,5 +1,14 @@
-mport {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView} from 'react-native';
 import React from 'react';
 
 const Aboutus = () => {
-  return()
+  return(
+    <SafeAreaView style={{backgroundColor: '#AF9FB2'}}>
+      <Text
+        style={{
+          fontSize: 20,
+          marginLeft:10,
+          marginTop: 20,
+        }}>
+        ABOUT US
+      </Text>

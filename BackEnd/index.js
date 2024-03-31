a6873f5d1357866d10430362d64c298dabc19dbf
NadeepaThemiya <nadeepathushal@gmail.com>
2024-03-31 13:11:36 +0530

    Finalized login/signup backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -77,3 +77,65 @@ app.post('/classes', async (req, res) => {
     }
 
 })
+
+app.get('/all-classes', async (req, res) => {
+    try {
+        await dbConnect();
+        const data = await ClassModel.find({})
+        console.log(data)
+        res.json({ message: "success", data });
+    } catch (error) {
+        console.log(error)
+        res.json({ message: "error" });
+    }
+
+})
+
+app.get('/classes', async (req, res) => {
+    try {
+        await dbConnect();
+
+        const threeDaysFromNow = moment().add(3, 'days').toDate();
+
+        // Query to find classes within 3 days from now
+        const data = await ClassModel.find({
+            date: {
+                $gte: new Date(),
+                $lte: threeDaysFromNow
+            }
+        });
+
+        console.log(data);
+        res.json({ message: "success", data });
+    } catch (error) {
+        console.error(error);
+        res.json({ message: "error" });
+    }
+});
+
+app.get('/classes/:className', async (req, res) => {
+    try {
+        await dbConnect();
+
+        const className = req.params.className;
+
+
+        const currentDate = new Date();
+        const threeDaysLater = moment(currentDate).add(3, 'days').toDate();
+
+
+        const data = await ClassModel.find({
+            class: className,
+            date: {
+                $gte: currentDate,
+                $lte: threeDaysLater
+            }
+        });
+
+        console.log(data);
+        res.json({ message: "success", data });
+    } catch (error) {
+        console.log(error);
+        res.json({ message: "error" });
+    }
+});

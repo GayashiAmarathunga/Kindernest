ba4d0d5bfd7b3eff73457f394a25270c9f0575f0
NadeepaThemiya <nadeepathushal@gmail.com>
2024-04-01 03:29:41 +0530

    Update ParentSignup.tsx

--- Frontend/Components/Screens/ParentSignup.tsx
+++ Frontend/Components/Screens/ParentSignup.tsx
@@ -1,3 +1,14 @@
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { supabase } from '../../lib/supabase'
+
+
+const ParentSignup = ({ navigation }) => {
+    const [parentName, setParentName] = useState('')
+    const [childName, setChildName] = useState('')
+    const [kindergartenLevel, setKindergartenLevel] = useState('')
+    const [classLevel, setClassLevel] = useState('')
+    const [dateOfBirth, setDateOfBirth] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
